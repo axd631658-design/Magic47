3933af8835cfab7e149c47dd34ee67a7c30cf679
axd631658-design <axd631658@gmail.com>
2026-07-18 23:32:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -56,6 +56,10 @@ if st.button("Create Reel"):
         st.write(f"🎙️ Voice Duration: {total_voice:.2f} sec")
         st.write(f"📹 Total Clips: {total_clips}")
         st.write(f"⏱️ Each Clip: {clip_time:.2f} sec")
+        st.subheader("🤖 AI Reel Plan")
+
+for i in range(total_clips):
+    st.write(f"Clip {i+1} ➜ {clip_time:.2f} sec")
 # Show Uploaded Video Durations
 if video:
     st.write("Uploaded Videos:")
